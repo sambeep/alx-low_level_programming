793094d259aee4be805ba093b0bd6a102c9d52df
sambeep <samsonkinyanjui11@gmail.com>
2022-06-07 05:36:02 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+
+
+
+/**
+* a program that prints _putchar
+* Return: On success 1.
+* On error, -1 is returned, and errno is set appropriately.
+*/
+int _putchar(char c)
+{
+return (write(1, &c, 1));
+}
