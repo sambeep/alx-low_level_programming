1b0ff4130a43c69513d372116c6bcf014afd9f67
sambeep <samsonkinyanjui11@gmail.com>
2022-06-08 08:06:00 -0700

    the alphabet game

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+
+#include "main.h"
+
+/**
+*print_alphabet - entry point
+* Description: Prints the alphabet with _putchar
+*
+* Return: void
+*/
+void print_alphabet(void)
+{
+void print_alphabet(void)
+
+for (c = a; c <= z; c++)
+
+{
+_putchar(c);
+}
+_putchar(n);
+
+}
