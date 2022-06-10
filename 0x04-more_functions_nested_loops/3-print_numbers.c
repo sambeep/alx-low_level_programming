01429312a3114c5402912ac932fafa7a64c041f0
sambeep <samsonkinyanjui11@gmail.com>
2022-06-10 10:25:39 -0700

    numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,14 +1,16 @@
 #include "main.h"
 /**
- * print_numbers - prints 0123456789\n
- * Return: void
- */
+* print_numbers - entry point
+* Description: Prints the numbers with _putchar
+* Return: void
+*/
+
 void print_numbers(void)
 {
-	char c;
-	for (c = 0; c <= 9; c++)
+	int number;
+	for (number = 0; number <= 9; number++)
 	{
-		_putchar(c);
+		_putchar(number);
 	}
 	_putchar(n);
 }
