6e9ccc1737b017cd738dd5a15183ed05c4238867
sambeep <samsonkinyanjui11@gmail.com>
2022-06-11 05:14:54 -0700

    Learn your times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,6 +10,7 @@ void times_table(void)
 
 {
 	int rone, cone, d;
+
 	for (rone = 0; rone <= 9; rone++)
 	{
 		_putchar('0');
@@ -28,6 +29,7 @@ void times_table(void)
 				_putchar(' ');
 			}
 			_putchar((d % 10) + '0');
+			
 			if (cone < 9)
 			{
 				_putchar(',');
