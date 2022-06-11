4c6135b8ef9fdbf81592e3cd1413982eae715a22
sambeep <samsonkinyanjui11@gmail.com>
2022-06-11 01:00:56 -0700

    0. _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,22 @@
 #include "main.h"
 /**
-* main - main block
-* Description: print _putchar as out put
-* Return: 0
-*/
+ * main - main block
+ * Description: print _putchar as out put
+ * Return: 0
+ */
+
 int main(void)
 {
-char a[] = "_putchar";
-int i;
-/* while loop*/
-i = 0;
-while (a[i] != 0)
-{
-_putchar(a[i]);
-i++;
-}
-_putchar (n);
-return (0);
+	char a[] = "_putchar";
+	int i;
+	
+	/* while loop*/
+	i = 0;
+	while (a[i] != 0)
+	{
+		_putchar(a[i]);
+		i++;
+	}
+	_putchar (n);
+	return (0);
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,7 +1,7 @@
-ifndef _putchar(n);
-define _putchar(n);
-int _putchar(char c);
+#ifndef _putchar(n);
+#define _putchar(n);
 
+int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
