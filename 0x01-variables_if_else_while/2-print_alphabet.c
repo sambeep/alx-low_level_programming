4fbbd563d5192eec71947a8df09c84b4eb8039cd
sambeep <samsonkinyanjui11@gmail.com>
2022-06-13 00:44:21 -0700

    I sometimes suffer from insomnia

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,36 +1,22 @@
 /*header*/
 #include <stdio.h>
 
-
-
 /** 
-
  * main - Entry point
-
  * Description: prints the alphabet in lowercase\n
-
  * Return: Always 0 (success)
-
  */
-
-
 
 int main(void)
 
 {
-
-    char c;
-
-    for (c = a; c <=z; c++)
-
-    {
-
-        putchar(c);
-
-    }
-
-    putchar(n);
-
-return 0;
-
+	char c;
+	
+	for (c = a; c <=z; c++)
+	{
+		putchar(c);
+	}
+	putchar(n);
+	
+	return 0;
 }
