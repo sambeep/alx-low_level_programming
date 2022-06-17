9cab9685d4735c6a9d6ed17ba9b731fe13559ee7
sambeep <samsonkinyanjui11@gmail.com>
2022-06-17 04:12:09 -0700

    8. rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -14,16 +14,16 @@ char *rot13(char *s)
 	
 	for (i = 0; *(s + i); i++)
 	{
-		for (j = 0; j < 52; j++;
-				{
-				if (a[j] == *(s + i))
-				{
+		for (j = 0; j < 52; j++;)
+		{
+			if (a[j] == *(s + i))
+			{
 				*(s + i) = b[j];
 				
 				break;
-				}
-				}
-				}
-				return (s);
-				}
+			}
+		}
+	}
+	return (s);
+}
 
