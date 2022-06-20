9d68a1ca63490651361ee3f234c44b561342f740
sambeep <samsonkinyanjui11@gmail.com>
2022-06-20 03:52:55 -0700

    5. strstr

---
+++ 0x07-pointers_arrays_strings/5-main.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check
+ * Return: Always 0
+ */
+
+int main(void)
+
+{
+	char *s = "hello, world";
+	char *f = "world";
+	char *t;
+	
+	t = _strstr(s, f);
+	printf("%s\n", t);
+	
+	return (0);
+}
