9c2b8ffea220773b73bf9262f2a63ca8a6be0c2f
sambeep <samsonkinyanjui11@gmail.com>
2022-07-05 00:38:40 -0700

    4. A door is what a dog is perpetually on the wrong side of

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,6 +1,5 @@
 #include <stdlib.h>
 #include "dog.h"
-
 /**
  * new_dog - Creates a new dog
  * @name: New name of dog
@@ -9,41 +8,35 @@
  * Return: Pointer to new dog struct
  * NULL if the function fails
  */
-
 dog_t *new_dog(char *name, float age, char *owner)
-
 {
-	int i, nameLen = 0, ownLen = 0;
-	dog_t *newDog;
-	
-	newDog = malloc(sizeof(dog_t));
-	if (newDog == NULL)
-	{
-		free(newDog);
-		return (NULL);
-	}
-	for (i = 0; name[i] != '\0'; i++)
-		nameLen++;
-	ewDog->name = malloc(nameLen + 1);
-	if (newDog->name == NULL)
-	{
-		free(newDog->name), free(newDog);
-		return (NULL);
-	}
-	for (i = 0; owner[i] != '\0'; i++)
-		ownLen++;
-	newDog->owner = malloc(ownLen + 1);
-	if (newDog->owner == NULL)
-	{
-		free(newDog->owner), free(newDog->name), free(newDog);
-		return (NULL);
-	}
-	newDog->age = age;
-	for (i = 0; i < nameLen; i++)
-		newDog->name[i] = name[i];
-	newDog->name[i] = '\0';
-	for (i = 0; i < ownLen; i++)
-		newDog->owner[i] = owner[i];
-	newDog->owner[i] = '\0';
-	return (newDog);
+int i, nameLen = 0, ownLen = 0;
+dog_t *newDog;
+newDog = malloc(sizeof(dog_t));
+if (newDog == NULL)
+{
+free(newDog);
+return (NULL);
+}
+for (i = 0; name[i] != '\0'; i++)
+nameLen++;
+newDog->name = malloc(nameLen + 1);
+if (newDog->name == NULL)
+{
+free(newDog->name), free(newDog);
+return (NULL);
+}
+for (i = 0; owner[i] != '\0'; i++)
+ownLen++;
+newDog->owner = malloc(ownLen + 1);
+if (newDog->owner == NULL)
+{
+free(newDog->owner), free(newDog->name), free(newDog);
+return (NULL);
+}
+newDog->age = age;
+for (i = 0; i < nameLen; i++)
+newDog->name[i] = name[i];
+newDog->name[i] = '\0';															for (i = 0; i < ownLen; i++)														newDog->owner[i] = owner[i];
+newDog->owner[i] = '\0';														return (newDog);
 }
