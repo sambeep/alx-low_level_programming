2bd2d4b22be009c001328cafaaca9e99d76b160a
sambeep <samsonkinyanjui11@gmail.com>
2022-07-22 06:29:30 -0700

    federal agent Jack Bauer

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,17 +1,14 @@
 #include "main.h"
 
-/**jack_bauer - function that prints every minute of the day, 24 hour clock
- * h = hour, m = minutes
- * / 10 allows second digit to rotate
- * for loop breaks before passing 24:00
- * Return: 24 hour clock line by line
+/**
+ * jack_bauer - prints time
+ * Description: prints time including hours and minutes
+ * Return: sum of two  numbers
  */
-
 void jack_bauer(void)
-
 {
 	int h, m;
-	
+
 	for (h = 0; h < 24; h++)
 	{
 		for (m = 0; m < 60; m++)
