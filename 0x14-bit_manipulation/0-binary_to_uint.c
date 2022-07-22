371fcf890ab48ac0203e59b2b25a873db05e2015
sambeep <samsonkinyanjui11@gmail.com>
2022-07-22 06:15:50 -0700

    0. 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,24 +1,48 @@
+#include "main.h"
+
 /**
- * binary_to_uint - convert binary string to decimal
- * @b: binary string
- *
- * Return: decimal (unsigned int)
+ * _stoi - converts chars to ints
+ * @c: char to convert
+ * Return: converted int
  */
 
-unsigned int binary_to_uint(const char *b)
+unsigned int _stoi(char c)
 {
-	unsigned int decimal;
+	return ((unsigned int) c - '0');
+}
+/**
+ * _strlen - calculates the length of the string
+ * @s: input
+ * Return: length of string
+ */
+unsigned int _strlen(const char *s)
+{
 	unsigned int i;
 
-	for (decimal = 0, i = 0; b[i] != '\0'; i++)
-	{
-		if (b[i] == '1')
-			decimal = (decimal << 1) | 1;
-		else if (b[i] == '0')
-			decimal <<= 1;
-		else if (b[i] != '0' && b[i] != '1')
-			return (0);
-	}
+	for (i = 0; s[i]; i++)
+		;
+	return (i);
+}
+/**
+ * binary_to_uint - converts a string of 1's and 0's to a decimal number
+ * @b: string to convert
+ * Return: unsigned decimal number
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	int i;
+	unsigned int result, tmp, expo;
 
-	return (decimal);
+	if (!b)
+		return (0);
+	result = tmp = 0;
+	expo = 1;
+	for (i = _strlen(b) - 1; b[i]; i--, expo *= 2)
+	{
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
+		tmp = _stoi(b[i]);
+		result += tmp * expo;
+	}
+	return (result);
 }
