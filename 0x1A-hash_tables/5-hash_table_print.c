8543c64edb834e08205f2084eb196bd80d355fd1
sambeep <samsonkinyanjui11@gmail.com>
2022-09-22 23:06:46 -0700

    5. >>> print(ht) mandatory Write a function that prints a hash table.
    Prototype: void hash_table_print(const hash_table_t *ht);
    where ht is the hash table
    You should print the key/value in the order that they appear in the array of hash table
    Order: array, list
    Format: see example
    If ht is NULL, don’t print anything

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -1,31 +1,36 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_print - prints a hash table
- * @ht: hash table to print
+ * hash_table_print - Print whole the hash table.
+ * @ht: Hash table.
  *
- * Return: void
+ * Return: Void.
  */
 void hash_table_print(const hash_table_t *ht)
 {
-	unsigned long int i;
-	hash_node_t *tmp;
-	char flag = 0; /* 0 while no data has been printed yet */
+	unsigned long int i, flag = 0;
+	hash_node_t *current;
 
-	if (ht == NULL || ht->array == NULL)
+	if (ht == NULL)
 		return;
-	printf("{");
+	putchar('{');
 	for (i = 0; i < ht->size; i++)
 	{
-		tmp = ht->array[i];
-		while (tmp != NULL)
+		current = ht->array[i];
+		if (current != NULL)
 		{
-			if (flag == 1)
-				printf(", ");
-			printf("'%s': '%s'", tmp->key, tmp->value);
-			flag = 1;
-			tmp = tmp->next;
+			while (current != NULL)
+			{
+				if (flag == 1)
+				{
+					printf(", ");
+				}
+				printf("'%s': '%s'", current->key, current->value);
+				flag = 1;
+				current = current->next;
+			}
 		}
 	}
-	printf("}\n");
+	putchar('}');
+	putchar('\n');
 }
