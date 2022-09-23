cf99993cab3af6c103c7d47b409357d49c8f5302
sambeep <samsonkinyanjui11@gmail.com>
2022-09-22 23:08:56 -0700

    6. >>> del ht mandatory Write a function that deletes a hash table.
    Prototype: void hash_table_delete(hash_table_t *ht);
    where ht is the hash table

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -1,31 +1,30 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_delete - deletes a hash table
- * @ht: hash table to delete
+ * hash_table_delete - Delete the hash table.
+ * @ht: Hash table.
  *
- * Return: void
+ * Return: Void.
  */
 void hash_table_delete(hash_table_t *ht)
 {
 	unsigned long int i;
-	hash_node_t *next;
+	hash_node_t *current;
 
-	if (ht == NULL || ht->array == NULL || ht->size == 0)
-		return;
 	for (i = 0; i < ht->size; i++)
 	{
-		while (ht->array[i] != NULL)
+		if (ht->array[i] != NULL)
 		{
-			next = ht->array[i]->next;
-			free(ht->array[i]->key);
-			free(ht->array[i]->value);
-			free(ht->array[i]);
-			ht->array[i] = next;
+			while (ht->array[i] != NULL)
+			{
+				current = ht->array[i]->next;
+				free(ht->array[i]->key);
+				free(ht->array[i]->value);
+				free(ht->array[i]);
+				ht->array[i] = current;
+			}
 		}
 	}
 	free(ht->array);
-	ht->array = NULL;
-	ht->size = 0;
 	free(ht);
 }
